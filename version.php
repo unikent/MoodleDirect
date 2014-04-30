f530772e88520ea084381b5a4df8abefe5f7c2a8
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-04-30 08:47:07 +0100

    Version Bump

--- version.php
+++ version.php
@@ -8,7 +8,7 @@ if (!isset($plugin)) {
 	$plugin = new stdClass();
 }
 
-$plugin->version  = 2014042200;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2014043000;  // The current module version (Date: YYYYMMDDXX)
 $plugin->cron     = 1800;        // Period for cron to check this module (secs)
 //$plugin->requires = 2007101509;  // 1.9+
 $plugin->requires = 2010112400;  // 2.0+
